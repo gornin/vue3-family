82362c10bca25699e0e8049d66cd96344f23832e
gnkevin <gnkevin@163.com>
2023-03-08 13:57:35 +0800

    feat: 尝试找出报错文件所在

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,9 +13,9 @@ export default defineConfig({
     }),
     Components({
       resolvers: [ElementPlusResolver()],
-    })],
-    server: {
-      host: '0.0.0.0'
-      // 0.0.0.0
-    },
-})
+    }),
+  ],
+  server: {
+    host: "127.0.0.1",
+  },
+});
